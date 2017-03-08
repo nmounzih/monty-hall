512cad0c614ad53dcdd14b8e3b6ce4d84c67f55c
Nadia Mounzih <nmounzih@yahoo.com>
2017-03-08 17:06:47 -0500

    Compressed all three functions into one function

--- monty_hall.py
+++ monty_hall.py
@@ -1,15 +1,21 @@
 import random
 
 
-def stay_game_simulation():
+def main():
     stay_goat_prizes = 0
     stay_car_prizes = 0
+    switch_goat_prizes = 0
+    switch_car_prizes = 0
+    random_goat_prizes = 0
+    random_car_prizes = 0
     for x in range(1, 1000):
         door_car = []
         door_first_pick = []
+        door_second_pick = []
         poss_goat = []
         door_goat = []
         doors = [1, 2, 3]
+        random_pool = []
         car = random.choice(doors)
         door_car.append(car)
         pick = random.choice(doors)
@@ -22,25 +28,6 @@ def stay_game_simulation():
             stay_car_prizes += 1
         else:
             stay_goat_prizes += 1
-    print("\nWhen you choose to stay: \n")
-    print("\tGoats: {}".format(stay_goat_prizes))
-    print("\tCars: {}\n".format(stay_car_prizes))
-
-
-def switch_game_simulation():
-    switch_goat_prizes = 0
-    switch_car_prizes = 0
-    for x in range(1, 1000):
-        door_car = []
-        door_first_pick = []
-        door_second_pick = []
-        poss_goat = []
-        door_goat = []
-        doors = [1, 2, 3]
-        car = random.choice(doors)
-        door_car.append(car)
-        pick = random.choice(doors)
-        door_first_pick.append(pick)
         for num in doors:
             if num not in door_car and num not in door_first_pick:
                 poss_goat.append(num)
@@ -52,30 +39,6 @@ def switch_game_simulation():
             switch_car_prizes += 1
         else:
             switch_goat_prizes += 1
-    print("When you choose to switch: \n")
-    print("\tGoats: {}".format(switch_goat_prizes))
-    print("\tCars: {}\n".format(switch_car_prizes))
-
-
-def random_choice_game_simulation():
-    random_goat_prizes = 0
-    random_car_prizes = 0
-    for x in range(1, 1000):
-        door_car = []
-        door_first_pick = []
-        door_second_pick = []
-        random_pool = []
-        poss_goat = []
-        door_goat = []
-        doors = [1, 2, 3]
-        car = random.choice(doors)
-        door_car.append(car)
-        pick = random.choice(doors)
-        door_first_pick.append(pick)
-        for num in doors:
-            if num not in door_car and num not in door_first_pick:
-                poss_goat.append(num)
-        door_goat.append(random.choice(poss_goat))
         for num in doors:
             if num not in door_goat and num not in door_first_pick:
                 door_second_pick.append(num)
@@ -86,12 +49,16 @@ def random_choice_game_simulation():
             random_car_prizes += 1
         else:
             random_goat_prizes += 1
+
+    print("\nWhen you choose to stay: \n")
+    print("\tGoats: {}".format(stay_goat_prizes))
+    print("\tCars: {}\n".format(stay_car_prizes))
+    print("When you choose to switch: \n")
+    print("\tGoats: {}".format(switch_goat_prizes))
+    print("\tCars: {}\n".format(switch_car_prizes))
     print("When you choose random: \n")
     print("\tGoats: {}".format(random_goat_prizes))
-    print("\tCars: {}".format(random_car_prizes))
+    print("\tCars: {}\n".format(random_car_prizes))
 
 
-
-stay_game_simulation()
-switch_game_simulation()
-random_choice_game_simulation()
+main()
