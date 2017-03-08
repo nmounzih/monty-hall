dedbe1219242c976836cdb0240c0578cea169293
Nadia Mounzih <nmounzih@yahoo.com>
2017-03-08 16:07:46 -0500

    Got switch strategy to work

--- monty_hall.py
+++ monty_hall.py
@@ -1,9 +1,9 @@
 import random
 
 
-def game_simulation():
-    goat_prizes = 0
-    car_prizes = 0
+def stay_game_simulation():
+    stay_goat_prizes = 0
+    stay_car_prizes = 0
     for x in range(1, 1000):
         door_car = []
         door_first_pick = []
@@ -19,13 +19,42 @@ def game_simulation():
                 poss_goat.append(num)
         door_goat.append(random.choice(poss_goat))
         if door_first_pick == door_car:
-            car_prizes += 1
+            stay_car_prizes += 1
         else:
-            goat_prizes += 1
-    print(goat_prizes)
-    print(car_prizes)
+            stay_goat_prizes += 1
+    print("Goats: {}".format(stay_goat_prizes))
+    print("Cars: {}\n".format(stay_car_prizes))
+
+
+def switch_game_simulation():
+    switch_goat_prizes = 0
+    switch_car_prizes = 0
+    for x in range(1, 1000):
+        door_car = []
+        door_first_pick = []
+        door_second_pick = []
+        poss_goat = []
+        door_goat = []
+        doors = [1, 2, 3]
+        car = random.choice(doors)
+        door_car.append(car)
+        pick = random.choice(doors)
+        door_first_pick.append(pick)
+        for num in doors:
+            if num not in door_car and num not in door_first_pick:
+                poss_goat.append(num)
+        door_goat.append(random.choice(poss_goat))
+        for num in doors:
+            if num not in door_goat and num not in door_first_pick:
+                door_second_pick.append(num)
+        if door_second_pick == door_car:
+            switch_car_prizes += 1
+        else:
+            switch_goat_prizes += 1
+    print("Goats: {}".format(switch_goat_prizes))
+    print("Cars: {}".format(switch_car_prizes))
 
 
 
-
-game_simulation()
+stay_game_simulation()
+switch_game_simulation()
